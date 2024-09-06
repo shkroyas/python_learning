19fd42b681ce931616e3c5513a57998dfbd72772
Royas Shakya <shkroyas@gmail.com>
2024-09-06 14:46:09 +0545

    Strings in Python

---
+++ strings.py
@@ -0,0 +1,12 @@
+name='Royas'
+print('Hello,\nHow are you?')
+print(name)
+print(name.upper())
+print(name.lower())
+print(name.isupper())
+print(name.islower())
+print(name.upper().isupper())
+print(name.lower().islower())
+print(len(name))
+print(name.index('y'))
+print(name.replace('s',`'l'))
