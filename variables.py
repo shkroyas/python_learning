1609f141f7cb977846e3b6acfc85d27b074479e2
Royas Shakya <shkroyas@gmail.com>
2024-09-06 14:26:37 +0545

    Variables in Python

---
+++ variables.py
@@ -0,0 +1,7 @@
+name = 'Royas'
+age = 18
+
+print (name+' is a boy')# We can use + only for strings
+print (name,' is ', age)# We can use , for integers and integers
+print (name, ' is from Nepal ' )
+print(name)
