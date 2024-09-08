442d1092450be3ec882118992e0940bcb8e4a5b0
Royas Shakya <shkroyas@gmail.com>
2024-09-08 23:28:11 +0545

    Numbers in Python

--- numbers.py
+++ numbers.py
@@ -1,1 +1,11 @@
-a=55
+number = 55
+number2 = str(number)
+print(20%6)
+print('number is '+ number2)
+print(-5)
+print(abs(-5))
+print(max(4, 2, 3, 16))
+print(min(4, 2, 3, 16))
+print(round(3.2))
+print(round(3.5))
+print(bin(3))
