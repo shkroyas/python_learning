4648ecc13c45665411640c8c18fc423c0898fc2e
Royas Shakya <shkroyas@gmail.com>
2024-09-09 16:04:03 +0545

    Tuples in Python

---
+++ tuple.py
@@ -0,0 +1,6 @@
+three_numbers = (1, 2, 3, 1)
+strings = ('home', 'land','earth')
+boo = (True, False, True)
+print(type(three_numbers[0]))
+print(type(three_numbers))
+#Mostly we use Lists but Tuples can be best when we want to use coordinates that should not change
