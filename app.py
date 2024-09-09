16b144f1222dd6cd1b9742edbe03207307256971
Royas Shakya <shkroyas@gmail.com>
2024-09-09 14:41:09 +0545

    word repalcement

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
-name = input('Input Your Name: ')
-age = int(input('Input Your Age: '))
-print('Your name is ' + name + ' and you are ', age)
+
+sentence = input('Enter your sentence:')
+print('Your sentence is: '+sentence)
+word1 = input('Enter the word to replace: ')
+word2 = input('Enter the word to replace it with: ')
+print(sentence.replace(word1, word2))
