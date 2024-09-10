f2409cd108da50bb1bf7d663df32ebfb7c9f4b24
Royas Shakya <shkroyas@gmail.com>
2024-09-10 09:33:05 +0545

    Dictionaries in Python

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+my_dict = {
+    'name': 'Royas',
+    'nationality': 'Nepali',
+    'age': 19,
+    'is_tall': True,
+    'Qualification': 'College',
+    'friends':['Rohan','Bibek']
+}
+x= my_dict['name']
+print(x)
+print(len(my_dict))
+print(type(my_dict))
