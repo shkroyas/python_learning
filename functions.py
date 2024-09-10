b92f70460ff82b0b4cac12b1017e6faa6b99720d
Royas Shakya <shkroyas@gmail.com>
2024-09-10 09:12:12 +0545

    Functions in Python

---
+++ functions.py
@@ -0,0 +1,7 @@
+def greetings_functions(name, age):
+    print('Welcome '+name+ '. You are '+str(age)+' years old.')
+
+greetings_functions('Royas', 19)
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+greetings_functions(name, age)
